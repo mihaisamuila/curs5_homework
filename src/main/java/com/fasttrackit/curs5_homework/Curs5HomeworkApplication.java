6872c535f284e7e1226ab5ccd67c111244f9cd02
samui <samuila.mihai.g@gmail.com>
2021-04-04 17:08:33 +0300

    test

---
+++ src/main/java/com/fasttrackit/curs5_homework/Curs5HomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.fasttrackit.curs5_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Curs5HomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Curs5HomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fasttrackit/curs5_homework/Curs5HomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fasttrackit.curs5_homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Curs5HomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
